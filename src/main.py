806becc3e35f4a48aaf83bc6a4a0e2e0168150cd
nickolasvm <nickolas64vm@gmail.com>
2024-06-06 12:07:56 -0300

    feat(error-handling): Added error handling to fetch_results_by_date method.

--- src/main.py
+++ src/main.py
@@ -5,16 +5,20 @@ def import_csv_to_database(conn):
     try:
         csv_file = 'data/input/cad_cia_aberta.csv'
         db.insert_data_from_csv(conn, csv_file)
-        print('Arquivo importado com sucesso!')
+        print('\nArquivo importado com sucesso!')
     except FileNotFoundError:
         print('''
-              Arquivo .csv não encontrado!
-              Certifique-se que o arquivo existe na pasta "data/input"''')
+        Arquivo .csv não encontrado!
+        Certifique-se que o arquivo existe na pasta "data/input"
+        ''')
 
 
 def fetch_results_by_date(conn):
-    date_str = input('Insira uma data para pesquisar (DD/MM/YYYY): ')
-    db.fetch_by_date(conn, date_str)
+    try:
+        date_str = input('\nInsira uma data para pesquisar (DD/MM/YYYY): ')
+        db.fetch_by_date(conn, date_str)
+    except ValueError:
+        print("\nData inválida!")
 
 
 def main():
@@ -25,7 +29,7 @@ def main():
     db.initialize_database(conn)
 
     while True:
-        print('\nOpções:')
+        print('\nSelecione uma das opções:\n')
         print('1. Importar arquivo .csv para o banco de dados')
         print('2. Pesquisar cias por data')
         print('3. Sair')
@@ -39,7 +43,7 @@ def main():
         elif choice == '3':
             break
         else:
-            print('Escolha inválida. Tente novamente.')
+            print('\nEscolha inválida. Tente novamente.')
 
     # Close connection
     conn.close()
