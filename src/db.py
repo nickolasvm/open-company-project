0e1f011b6b987690e1c8873b56cfd1ec1c873528
nickolasvm <nickolas64vm@gmail.com>
2024-06-10 11:00:35 -0300

    refactor: added methods paginate_table and format_cnpj to a utils file.

--- src/db.py
+++ src/db.py
@@ -2,6 +2,7 @@ import sqlite3
 import pandas as pd
 from prettytable import from_db_cursor
 from datetime import datetime
+from utils import paginate_table, format_cnpj
 
 
 def create_connection(db_file):
@@ -99,43 +100,3 @@ def fetch_by_cnpj(conn, cnpj_str):
         print('\nNão existe dados para este CNPJ.')
     else:
         paginate_table(table)
-
-
-def paginate_table(table, page_size=100):
-    total_rows = len(table.rows)
-
-    # Don't need to paginate if there is less rows than page size
-    if (total_rows < page_size):
-        print(table)
-        return
-
-    #  Adding page_size - 1 ensures that the division will round up to the nearest integer
-    num_pages = (total_rows + page_size - 1) // page_size
-    current_page = 1
-
-    while True:
-        print('\n')
-        start_index = (current_page - 1) * page_size
-        end_index = min(total_rows, current_page * page_size)
-
-        print(table.get_string(start=start_index, end=end_index))
-
-        print(f'\nPágina {current_page}/{num_pages}')
-
-        user_input = input('Pressione Enter para continuar à próxima página (ou "q" para sair): ')
-        if user_input.lower() == 'q':
-            break
-        current_page += 1
-        if current_page > num_pages:
-            print('Fim dos dados.')
-            break
-
-
-def format_cnpj(cnpj_str):
-    # Split the string into segments
-    segments = [cnpj_str[:2], cnpj_str[2:5], cnpj_str[5:8], cnpj_str[8:12], cnpj_str[12:]]
-
-    # Join segments
-    formatted_cnpj = '{}.{}.{}/{}-{}'.format(*segments)
-
-    return formatted_cnpj
---
+++ src/utils.py
@@ -0,0 +1,38 @@
+def paginate_table(table, page_size=100):
+    total_rows = len(table.rows)
+
+    # Don't need to paginate if there is less rows than page size
+    if (total_rows < page_size):
+        print(table)
+        return
+
+    #  Adding page_size - 1 ensures that the division will round up to the nearest integer
+    num_pages = (total_rows + page_size - 1) // page_size
+    current_page = 1
+
+    while True:
+        print('\n')
+        start_index = (current_page - 1) * page_size
+        end_index = min(total_rows, current_page * page_size)
+
+        print(table.get_string(start=start_index, end=end_index))
+
+        print(f'\nPágina {current_page}/{num_pages}')
+
+        user_input = input('Pressione Enter para continuar à próxima página (ou "q" para sair): ')
+        if user_input.lower() == 'q':
+            break
+        current_page += 1
+        if current_page > num_pages:
+            print('Fim dos dados.')
+            break
+
+
+def format_cnpj(cnpj_str):
+    # Split the string into segments
+    segments = [cnpj_str[:2], cnpj_str[2:5], cnpj_str[5:8], cnpj_str[8:12], cnpj_str[12:]]
+
+    # Join segments
+    formatted_cnpj = '{}.{}.{}/{}-{}'.format(*segments)
+
+    return formatted_cnpj
